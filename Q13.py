73def16982246ce3389b6ec0f82086d81ece9835
akashadr <75382310+akashadr@users.noreply.github.com>
2021-06-12 12:33:33 +0530

    Delete Q13.py

--- Q13.py
+++
@@ -1,16 +0,0 @@
-if __name__=="__main__":
-  
-  N =  [[1, 1, 1],[0, 0, 1],[1, 1, 1]] 
-
-
-
-  
-  Count=0
-  
-  for i in range(len(N)):
-    for j in range(len(N[0])):
-      
-      if(N[i][j]==1):
-        Count+=1
-  
-  print(Count)
