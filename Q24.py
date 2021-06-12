77f701207f97888c0ba354ec656a4efedcc6e48a
akashadr <75382310+akashadr@users.noreply.github.com>
2021-06-12 12:30:39 +0530

    Delete Q24.py

--- Q24.py
+++
@@ -1,13 +0,0 @@
-if __name__=="__main__":
-  
-  L=[[1, 0, 0],[0, 1, 0],[0, 0, 1]]
-
-  for i in range(len(L)):
-    for j in range(len(L[0])):
-      
-      if(L[i][j]==1):
-        L[i][j]=0
-      else:
-        L[i][j]=1
-  
-  print(L)
