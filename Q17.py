de233acdf30dff4cf3f0fcdb8d36d18641716e75
akashadr <75382310+akashadr@users.noreply.github.com>
2021-06-12 12:34:00 +0530

    Delete Q17.py

--- Q17.py
+++
@@ -1,13 +0,0 @@
-if __name__=="__main__":
-  
-  N =  [[1, 0, 2],[5, 5, 7],[9, 4, 3]]
-
-  Sum=0
-  
-  for i in range(len(N)):
-    for j in range(len(N[0])):
-      
-      if(N[i][j]&1==0):
-        Sum+=N[i][j]
-  
-  print(Sum)
