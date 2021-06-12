8f82cf94f8b37d241c68fc1da3e839963b1b20ea
akashadr <75382310+akashadr@users.noreply.github.com>
2021-06-12 12:29:05 +0530

    Delete Q43.py

--- Q43.py
+++
@@ -1,12 +0,0 @@
-L=[[1,0,0,0],[0,5,0,2],[3,0,0,0],[0,0,4,0]]
-L1=[[1,0,0],[0,5,0],[0,0,0],[0,0,6]]
-
-
-print("Row Column Value")
-
-for i in range(len(L)):
-  for j in range(len(L[i])):
-    
-    if(L[i][j]!=0):
-       print(f"{i}      {j}     {L[i][j]}")
-    
